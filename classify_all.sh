245047039b6bf4feb839b39a6a52c5697cb04576
Jackson Woodruff <J.C.Woodruff@sms.ed.ac.uk>
2020-05-02 10:32:02 +0100

    Add a script to classify many different vectors

---
+++ classify_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ $# -lt 2 ]]; then
+	echo "Usage: $0 <output directory> <tests to run....>"
+	exit 1
+fi
+
+output=$1
+shift
+
+while [[ $# -gt 0 ]]; do
+	input=$1
+
+	# Need to keep each output folder unique wrt. each input folder.
+	./classify.sh $input ${output}${input}
+done
